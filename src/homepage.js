202cd0dc10c4f80eba65ea35b23446a4b144e253
Edwin <264506916+m-redw@users.noreply.github.com>
2026-06-12 16:33:17 -0700

    Create home page

---
+++ src/homepage.js
@@ -0,0 +1,84 @@
+import tempImage from "./temp-image.png";
+
+const content = document.querySelector('#content');
+
+function docCreate(element) {
+    return document.createElement(element);
+}
+
+function sectionCreate(className) {
+    const section = docCreate('section');
+    section.classList.add(className);
+    return section;
+} 
+
+function createHeader() {
+    const header = docCreate('h1');
+    header.textContent = "Ed's Eggs"
+    content.appendChild(header);
+}
+
+function createIntro() {
+    const section = sectionCreate('intro');
+
+    const image = docCreate('img');
+    image.src = tempImage;
+    image.style.width = '240px';
+    image.style.height = '135px';
+
+    const crack = docCreate('em');
+    const youUp = docCreate('span');
+    const p = docCreate('p');
+
+    p.textContent = `Hey, I'm Ed and I love me some eggs!
+                This restaurant has egg dishes galore!
+                From just quail eggs to omurice, the variety
+                of egg dishes may `;
+    crack.textContent = 'crack';
+    youUp.textContent = ' you up!'
+
+    p.appendChild(crack);
+    p.appendChild(youUp);
+
+    section.appendChild(image);
+    section.appendChild(p);
+    content.appendChild(section);
+}
+
+function createOpen() {
+    const section = sectionCreate('open');
+
+    const h2 = docCreate('h2');
+    h2.textContent = 'Open'
+
+    const pDays = docCreate('p');
+    const pHours = docCreate('p');
+    pDays.textContent = 'Mon - Thur';
+    pHours.textContent = '8am - 8pm';
+
+    section.appendChild(h2);
+    section.appendChild(pDays);
+    section.appendChild(pHours);
+    content.appendChild(section)
+}
+
+function createLocation() {
+    const section = sectionCreate('location');
+
+    const h2 = docCreate('h2');
+    h2.textContent = 'Location'
+
+    const pLocation = docCreate('p');
+    pLocation.textContent = '399 Egg St, Crackissipi, Yolkica';
+
+    section.appendChild(h2);
+    section.appendChild(pLocation);
+    content.appendChild(section)
+}
+
+export function loadHomepage() {
+    createHeader();
+    createIntro();
+    createOpen();
+    createLocation();
+}
--- src/index.js
+++ src/index.js
@@ -1,1 +1,18 @@
 import "./index.css";
+import {loadHomepage} from "./homepage.js";
+
+function clearContent() {
+    const content = document.querySelector('#content');
+
+    while (content.firstChild) {
+        content.firstChild.remove();
+    }
+}
+
+const homeButton = document.querySelector('.home');
+homeButton.addEventListener('click', ()=>{
+    clearContent();
+    loadHomepage();
+});
+
+loadHomepage();
